ca2f7170d4bd7c65a0c1586253317a4293d07656
Diana Murtazina <murka8831@yandex.ru>
2023-02-10 14:55:12 +0400

    adaptive for slider in contact page

--- app/js/main.js
+++ app/js/main.js
@@ -29,6 +29,64 @@ $(function () {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                },
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 7,
+                },
+            },
+            {
+                breakpoint: 1260,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                },
+            },
+            {
+                breakpoint: 1065,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                },
+            },
+            {
+                breakpoint: 890,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                },
+            },
+            {
+                breakpoint: 730,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     });
     $(".article-slider__box").slick({
         arrows: true,
